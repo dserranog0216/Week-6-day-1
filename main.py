17c7f76e0005a9a235551270119cf3ef15ed288a
David Serrano <113551943+dserranog0216@users.noreply.github.com>
2022-09-27 19:42:40 +0000

    Review Practice

--- main.py
+++ main.py
@@ -15,13 +15,21 @@
 seconds = [1.23, 1.45, 1.02]
 current = 1.11
 
+seconds.append(current)
+print(seconds)
+
 # Remove item 1.45 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
+seconds.remove(1.45)
+print(seconds)
 
 
 # Remove items 1.45, 1.02, and 1.11 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
+newSeconds = seconds [1:]
+seconds.remove(1.45)
 
+print(newSeconds)
 
 ################################comparison operators#########################
 #remember....
